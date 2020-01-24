7983761ba55bfc1adc8d6e85fefd4cba27dd416c
Paul Hammant <paul@hammant.org>
2020-01-24 21:46:39 +0000

    clue to an imminent step

--- spec/lib/climate_api_shared_example.rb
+++ spec/lib/climate_api_shared_example.rb
@@ -6,35 +6,47 @@ RSpec.shared_examples 'the Climate API' do
   context 'returns average rainfall from 1980 to 1999' do
     let(:delta) { 0.0000000001 }
 
-    it 'for Great Britain' do
+    it 'for Great Britain' do |ctx|
+      # This needs to be passed to the Servirtium playbacker so that it knows which markdown to load
+      puts "#{self.class.description} #{ctx.description}"
       result = climate_api.get_average_annual_rainfall(1980, 1999, 'gbr')
       expect(result).to be_within(delta).of 988.8454972331015
     end
 
-    it 'for France' do
+    it 'for France' do |ctx|
+      # This needs to be passed to the Servirtium playbacker so that it knows which markdown to load
+      puts "#{self.class.description} #{ctx.description}"
       result = climate_api.get_average_annual_rainfall(1980, 1999, 'fra')
       expect(result).to be_within(delta).of 913.7986955122727
     end
 
-    it 'for Egypt' do
+    it 'for Egypt' do |ctx|
+      # This needs to be passed to the Servirtium playbacker so that it knows which markdown to load
+      puts "#{self.class.description} #{ctx.description}"
       result = climate_api.get_average_annual_rainfall(1980, 1999, 'egy')
       expect(result).to be_within(delta).of 54.58587712129825
     end
 
-    it 'for both Great Britain & France combined' do
+    it 'for both Great Britain & France combined' do |ctx|
+      # This needs to be passed to the Servirtium playbacker so that it knows which markdown to load
+      puts "#{self.class.description} #{ctx.description}"
       result = climate_api.get_average_annual_rainfall(1980, 1999, 'gbr', 'fra')
       expect(result).to be_within(delta).of 951.3220963726872
     end
   end
 
   context 'average rainfall not supported' do
-    it 'for Great Britain from 1985 to 1995' do
+    it 'for Great Britain from 1985 to 1995' do |ctx|
+      # This needs to be passed to the Servirtium playbacker so that it knows which markdown to load
+      puts "#{self.class.description} #{ctx.description}"
       expect { climate_api.get_average_annual_rainfall(1985, 1995, 'gbr') }.to(
         raise_error(/not supported/)
       )
     end
 
-    it 'for Middle Earth' do
+    it 'for Middle Earth' do |ctx|
+      # This needs to be passed to the Servirtium playbacker so that it knows which markdown to load
+      puts "#{self.class.description} #{ctx.description}"
       expect { climate_api.get_average_annual_rainfall(1980, 1999, 'mde') }.to(
         raise_error(/Invalid country code/)
       )

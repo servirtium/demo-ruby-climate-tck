980b50a00b13f708e8d8c27252274d680abe464b
robpark <robert.d.park@gmail.com>
2020-02-06 10:32:24 -0500

    parse playback files for response

--- spec/lib/climate_api_playback_spec.rb
+++ spec/lib/climate_api_playback_spec.rb
@@ -18,22 +18,50 @@ RSpec.describe 'Climate API Playback' do
   end
 
   before(:each) do |example|
-    playback_name = "#{self.class.description} #{example.description}".downcase.gsub(' ', '_')
+    playback_name = "#{self.class.description}/#{example.description}".downcase.gsub(' ', '_')
     ServirtiumDemo.example = playback_name
   end
 
-  # Uncomment to see the current status
+  # Switch to the shared example here, once all tests below are passing
   # it_behaves_like 'the Climate API'
 
   # Specs for Debugging
 
-  it 'for Great Britain' do
-    result = climate_api.average_annual_rainfall(1980, 1999, 'gbr')
-    expect(result).to be_within(delta).of 988.8454972331015
+  context 'returns average rainfall from 1980 to 1999' do
+    let(:delta) { 0.0000000001 }
+
+    it 'for Great Britain' do
+      result = climate_api.average_annual_rainfall(1980, 1999, 'gbr')
+      expect(result).to be_within(delta).of 988.8454972331015
+    end
+
+    it 'for France' do
+      result = climate_api.average_annual_rainfall(1980, 1999, 'fra')
+      expect(result).to be_within(delta).of 913.7986955122727
+    end
+
+    it 'for Egypt' do
+      result = climate_api.average_annual_rainfall(1980, 1999, 'egy')
+      expect(result).to be_within(delta).of 54.58587712129825
+    end
+
+    xit 'for both Great Britain & France combined' do
+      result = climate_api.average_annual_rainfall(1980, 1999, 'gbr', 'fra')
+      expect(result).to be_within(delta).of 951.3220963726872
+    end
   end
 
-  it 'for France' do
-    result = climate_api.average_annual_rainfall(1980, 1999, 'fra')
-    expect(result).to be_within(delta).of 913.7986955122727
+  context 'average rainfall not supported' do
+    xit 'for Great Britain from 1985 to 1995' do
+      expect { climate_api.average_annual_rainfall(1985, 1995, 'gbr') }.to(
+        raise_error(/not supported/)
+      )
+    end
+
+    xit 'for Middle Earth' do
+      expect { climate_api.average_annual_rainfall(1980, 1999, 'mde') }.to(
+        raise_error(/Invalid country code/)
+      )
+    end
   end
 end

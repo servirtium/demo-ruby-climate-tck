f61b3b162fa4cc74fa09d9fb34b8c4cff4e7c079
robpark <robert.d.park@gmail.com>
2020-03-10 08:37:23 -0400

    Running multiple specs with single server before/after suite Note: playback/record specs are the same ATM

--- spec/lib/climate_api_record_spec.rb
+++ spec/lib/climate_api_record_spec.rb
@@ -5,7 +5,7 @@ require 'spec_helper'
 require 'demo_server'
 require 'servirtium_playback_servlet'
 
-RSpec.describe 'The Climate API playback (via Servirtium)' do
+RSpec.describe 'The Climate API record (via Servirtium)' do
   subject(:climate_api) { ClimateApiDemo::ClimateApi.new("http://127.0.0.1:#{PORT}") }
 
   let(:delta) { 0.0000000001 }
@@ -17,5 +17,14 @@ RSpec.describe 'The Climate API playback (via Servirtium)' do
   end
 
   # Switch to the shared example here, once all tests below are passing
-  it_behaves_like 'the Climate API in'
+  # it_behaves_like 'the Climate API in'
+
+  context 'returning average rainfall from 1980 to 1999' do
+    let(:delta) { 0.0000000001 }
+
+    it 'for Great Britain' do
+      result = climate_api.average_annual_rainfall(1980, 1999, 'gbr')
+      expect(result).to be_within(delta).of 988.8454972331015
+    end
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,22 @@
 
 require_relative '../lib/../lib/climate_demo'
 require 'rspec'
+PORT = 61_417
+
+RSpec.configure do |config|
+  $server_thread = nil
+
+  config.before(:suite) do
+    $server_thread = Thread.new do
+      Thread.current[:server] = ServirtiumDemo::DemoServer.new PORT
+      Thread.current[:server].start
+    end
+  end
+
+  config.after(:suite) do
+    s = $server_thread[:server]
+    s.stop
+    $server_thread.join 3
+    Thread.kill $server_thread
+  end
+end
